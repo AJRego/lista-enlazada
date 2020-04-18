7310f341e7decdf6c7b83e50724465728ce09d5f
AJRego <rego.andre99@gmail.com>
2020-04-18 16:36:25 -0500

    atributos y metodos de un nodo

--- src/metodos/nodo.java
+++ src/metodos/nodo.java
@@ -8,7 +8,32 @@ package metodos;
 /**
  *
  * @author ajrego
+ * @param <T>
  */
-public class nodo {
+public class nodo<T> {
+    private T dato;
+    private nodo<T> siguiente;
+
+    public nodo(T dato, nodo<T> siguiente) {
+        this.dato = dato;
+        this.siguiente = siguiente;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+    
     
 }
